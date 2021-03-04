0add27b8d6be9fc2693fff3c9d3e946276f3a381
Andrew Carrigan <carrigan223@gmail.com>
2021-03-04 12:54:49 -0800

    added magic

---
+++ context/AuthContext.js
@@ -0,0 +1,52 @@
+import { createContext, useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import { Magic } from "magic-sdk";
+import { MAGIC_PUBLIC_KEY } from "../utils/urls";
+
+const AuthContext = createContext();
+
+let magic;
+
+export const AuthProvider = (props) => {
+  const [user, setUser] = useState(null);
+  const router = useRouter();
+
+  /**
+   * Adds email to user
+   * @param {*} email
+   */
+
+  const loginUser = async (email) => {
+    try {
+      await magic.auth.loginWithMagicLink({ email });
+      setUser({ email });
+      router.push("/");
+    } catch (err) {
+      setUser(null);
+    }
+  };
+
+  /**
+   * Log the user out/sets the user to null
+   */
+
+  const logoutUser = async () => {
+    try {
+      await magic.user.logout();
+      setUser(null);
+      router.push("/");
+    } catch (err) {}
+  };
+
+  useEffect(() => {
+    magic = new Magic(MAGIC_PUBLIC_KEY);
+  });
+
+  return (
+    <AuthContext.Provider value={{ user, loginUser, logoutUser }}>
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContext;
--- utils/urls.js
+++ utils/urls.js
@@ -1,5 +1,7 @@
 export const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:1337";
 
+export const MAGIC_PUBLIC_KEY = process.env.NEXT_PUBLIC_MAGIC_PUBLIC_KEY || "pk_test_104FA06248AE6690";
+
 /**
  * Given an image return  the url
  * works for local and deployed strapis
