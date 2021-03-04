4079bc96d9f286b51f9e7a0dfae1d766f5315a9f
Andrew Carrigan <carrigan223@gmail.com>
2021-03-04 09:30:36 -0800

    added single product page

---
+++ utils/format.js
@@ -0,0 +1,8 @@
+
+/**
+ * Display the Number with 2 digits
+ * i.e. as a price 
+ * @param {string | number} number 
+ */
+
+export const twoDecimals = (number) => parseFloat(number).toFixed(2);
---
+++ utils/urls.js
@@ -0,0 +1,19 @@
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:1337";
+
+/**
+ * Given an image return  the url
+ * works for local and deployed strapis
+ * @param {any} img
+ */
+
+export const fromImageToUrl = (img) => {
+  if (!img) {
+    return "/vercel.svg";
+  }
+
+  if (img.url.indexOf("/") === 0) {
+    return `${API_URL}${img.url}`;
+  }
+
+  return img.url;
+};
