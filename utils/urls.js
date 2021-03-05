40c496408cbbd419f7265c5ae0d6c4e6ac102dc6
Andrew Carrigan <carrigan223@gmail.com>
2021-03-05 00:24:02 -0800

    added buy button

--- utils/urls.js
+++ utils/urls.js
@@ -1,7 +1,12 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:1337";
+export const API_URL =
+  process.env.NEXT_PUBLIC_API_URL || "http://localhost:1337";
 
-export const MAGIC_PUBLIC_KEY = process.env.NEXT_PUBLIC_MAGIC_PUBLIC_KEY || "pk_test_104FA06248AE6690";
+export const MAGIC_PUBLIC_KEY =
+  process.env.NEXT_PUBLIC_MAGIC_PUBLIC_KEY || "pk_test_104FA06248AE6690";
 
+export const STRIPE_PK =
+  process.env.NEXT_PUBLIC_STRIPE_PK ||
+  "pk_test_51HuDwYBlw6J01SCgwdlSllhYRhoBvsMXbGSQ8XU0bHwZgyDIqhdmODLEFow2HRGpv9ReLpQ8j5My9PehPcy5Zrjr00Sf3WEmkG";
 /**
  * Given an image return  the url
  * works for local and deployed strapis
